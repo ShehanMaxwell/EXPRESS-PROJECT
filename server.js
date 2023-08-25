6872b13f4e634387f3e9765280e2406f5c016281
Shehan Maxwell <shehanbbn@gmail.com>
2023-08-25 12:45:23 +0530

    modified server.js in .

--- server.js
+++ server.js
@@ -4,17 +4,20 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res)=>{
-  res.send('heeellooo')
-})
+app.get('/', (req, res) => {
+  res.send({
+    id: 1,
+    name: 'Rocky Balbo ',
+  });
+});
 
-app.get('/messages', (req, res)=>{
-  res.send('<ul><li>YO ADRIAN</li></ul>')
-})
+app.get('/messages', (req, res) => {
+  res.send('<ul><li>YO ADRIAN</li></ul>');
+});
 
-app.post('/messages' ,(req,res)=>{
+app.post('/messages', (req, res) => {
   console.log('Updating messages...');
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
