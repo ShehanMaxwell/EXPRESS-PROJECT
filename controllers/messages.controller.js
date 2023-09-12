e27aa175a4fb37622eea8bff1a64a39f82401de5
Shehan Maxwell <shehanbbn@gmail.com>
2023-09-12 14:28:28 +0530

    changed 4 files

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,3 +1,12 @@
 function getMessages(req, res) {
   res.send('<ul><li>YO ADRIAN</li></ul>');
 }
+
+function postMessages(req, res) {
+  console.log('Updating messages...');
+}
+
+module.exports = {
+  getMessages,
+  postMessages,
+};
