a1512feee28d11db6240bc7255088355c9e467e5
Shehan Maxwell <shehanbbn@gmail.com>
2023-09-12 19:15:48 +0530

    modified friends.router.js and messages.router.js in routes

--- routes/friends.router.js
+++ routes/friends.router.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
-const friendsController = require('./controllers/friends.controller');
+const friendsController = require('../controllers/friends.controller');
 
 const friendsRouter = express.Router();
+
+friendsRouter.use((req, res, next) => {
+  console.log('ip address:', req.ip);
+  next();
+});
 
 friendsRouter.post('/', friendsController.postFriend);
 friendsRouter.get('/', friendsController.getFriends);
