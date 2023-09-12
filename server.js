fb146e34a0c8aa8bf565cc6ce184386ab5b61e13
Shehan Maxwell <shehanbbn@gmail.com>
2023-09-12 19:19:44 +0530

    modified server.js in .

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
 
   next();
   const delta = Date.now() - start;
-  console.log(`${req.method} ${req.url} ${delta}ms`);
+  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
 app.use(express.json());
