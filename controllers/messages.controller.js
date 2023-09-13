6b7f5891e4ca65ad9aed12a99ea7040f16538fe9
Shehan Maxwell <shehanbbn@gmail.com>
2023-09-13 16:51:36 +0530

    changed 4 files

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,7 +1,11 @@
 const path = require('path');
 
 function getMessages(req, res) {
-  res.sendFile(path.join(__dirname, '..', 'public', 'images', 'last.jpg'));
+  res.render('messages', {
+    title: 'Messages to my fellow Gamers',
+    friend: 'Alexsa Parker',
+  });
+  // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'last.jpg'));
 }
 
 function postMessages(req, res) {
