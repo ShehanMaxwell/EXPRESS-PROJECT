374369b8dc9c2a4c97ee3798f1b106de6430304b
Shehan Maxwell <shehanbbn@gmail.com>
2023-09-13 15:39:05 +0530

    changed 3 files

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
-app.use(express.static('public'));
+app.use('/site', express.static('public'));
 app.use(express.json());
 
 app.use('/friends', friendsRouter);
